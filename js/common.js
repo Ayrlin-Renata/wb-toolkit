c19acdeb5a03c82e8e97c41c01042744c99fed03
ayrlin.renata <ayrlin.renata@gmail.com>
2020-07-04 07:06:31 +0000

    added recollapse function

--- js/common.js
+++ js/common.js
@@ -11,10 +11,20 @@ $(document).ready(function() {
 function initListeners() {
 	$('.collapse-row').click(function() {
 		doUncollapse(this);
+	}).find('.body-row').click(function() {
+		return false;
+	});
+	$('.collapse-arrow').click(function() {
+		doCollapse(this.closest('.collapse-row'));
 	});
 }
 
 function doUncollapse(ele) {
 	$(ele).find('.body-row').css("display","block");
 	$(ele).find('.collapse-header').css("display","none");
+}
+
+function doCollapse(ele) {
+	$(ele).find('.body-row').css("display","none");
+	$(ele).find('.collapse-header').css("display","block");
 }
